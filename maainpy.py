58433c6978b36bfdfe4b1ce649a5055804ee2cc0
Your Name <affan.grewal1234@gmail.com>
2026-02-28 16:15:53 +0500

    gfre

---
+++ maainpy.py
@@ -0,0 +1,9 @@
+class Vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+
+modelX=Vehicle(220,18)
+
+print("Model Max speed:",modelX.max_speed)
+print("Model Mileage",modelX.mileage)
